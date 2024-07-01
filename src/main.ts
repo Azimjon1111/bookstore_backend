d2b4bbb6dc3bbdef8e03d36cf53a305e411a8d89
Azimjon1111 <azimjon.nodir@gmail.com>
2024-07-01 12:14:47 +0500

    import packages ..

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      credentials: true,
+      origin: true,
+    },
+  })
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
