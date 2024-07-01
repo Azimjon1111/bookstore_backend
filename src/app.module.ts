4f8fa98e407382f52b9a4c38d20e1eeef49a20bf
Azimjon1111 <azimjon.nodir@gmail.com>
2024-07-01 15:06:36 +0500

    MONGO DB middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   providers: [],
 })
 export class AppModule {}
