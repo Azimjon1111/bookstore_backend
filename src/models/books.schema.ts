afd5e07c1257ecfc6d36a07fc780390d51a0b66a
Azimjon1111 <azimjon.nodir@gmail.com>
2024-07-01 12:25:17 +0500

    books schema created

---
+++ src/models/books.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory,  } from '@nestjs/mongoose'
+import { HydratedDocument , Types} from 'mongoose'
+
+export type BookDocument = HydratedDocument<Book>
+@Schema({ collection: 'books', timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } })
+export class Book {
+  @Prop()
+  name: string
+
+  @Prop()
+  description: string
+
+  @Prop({ type: Date})
+  published_date: Date;
+
+  @Prop({ref:'authors', type: Types.ObjectId, default: null, required: true})
+  author: Types.ObjectId;
+
+  @Prop()
+  rating: number
+
+  @Prop()
+  cover: string
+
+  @Prop()
+  genres: string
+
+  @Prop()
+  tags: string
+}
+
+export const BooksSchema = SchemaFactory.createForClass(Book)
+BooksSchema.index({
+  name: 'text',
+  description: 'text',
+})
